5e052fce7dffe7f47872bbd558459b7c563800e9
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-19 22:18:10 +0100

    feat: list dir

--- internal/localfs/localfs.go
+++ internal/localfs/localfs.go
@@ -1,6 +1,10 @@
 package localfs
 
-import "time"
+import (
+	"os"
+	"path/filepath"
+	"time"
+)
 
 type FileEntry struct {
 	Name        string    `json:"name"`
@@ -9,4 +13,29 @@ type FileEntry struct {
 	IsDir       bool      `json:"isDir"`
 	ModTime     time.Time `json:"modTime"`
 	Permissions string    `json:"permissions"`
+}
+
+func ListDir(path string) ([]FileEntry, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	files := make([]FileEntry, 0, len(entries))
+	for _, e := range entries {
+		info, err := e.Info()
+		if err != nil {
+			continue
+		}
+		files = append(files, FileEntry{
+			Name:        e.Name(),
+			Path:        filepath.Join(path, e.Name()),
+			Size:        info.Size(),
+			IsDir:       e.IsDir(),
+			ModTime:     info.ModTime(),
+			Permissions: info.Mode().String(),
+		})
+	}
+
+	return files, nil
 }
