5c0c68b3c74f0f2447efde3bfb40719b0b68ecd4
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-19 22:16:20 +0100

    feat: FileEntry struct

---
+++ internal/localfs/localfs.go
@@ -0,0 +1,12 @@
+package localfs
+
+import "time"
+
+type FileEntry struct {
+	Name        string    `json:"name"`
+	Path        string    `json:"path"`
+	Size        int64     `json:"size"`
+	IsDir       bool      `json:"isDir"`
+	ModTime     time.Time `json:"modTime"`
+	Permissions string    `json:"permissions"`
+}
