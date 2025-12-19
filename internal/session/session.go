3b5e56f6cd973485eb7cd5bef7915d8b8bb3b6ac
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-19 22:22:49 +0100

    feat: initialization

--- internal/session/session.go
+++ internal/session/session.go
@@ -11,3 +11,17 @@ type Session struct {
 	mu          sync.RWMutex
 	downloadDir string
 }
+
+var (
+	current *Session
+	once    sync.Once
+)
+
+func Initialize(client *sftp.Client, downloadDir string) {
+	once.Do(func() {
+		current = &Session{
+			client:      client,
+			downloadDir: downloadDir,
+		}
+	})
+}
