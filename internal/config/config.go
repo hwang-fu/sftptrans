09c6bf0e7810788be3cbda3b47d9ca4f1ac23a78
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-19 21:13:04 +0100

    fix: typo "host" -> "port"

--- internal/config/config.go
+++ internal/config/config.go
@@ -24,7 +24,7 @@ func Parse() (*Config, error) {
 
 	flag.StringVar(&cfg.User, "user", "", "SFTP host username (required)")
 	flag.StringVar(&cfg.Host, "host", "", "SFTP host address (required)")
-	flag.IntVar(&cfg.Port, "host", 22, "SFTP port (optional, default 22)")
+	flag.IntVar(&cfg.Port, "port", 22, "SFTP port (optional, default 22)")
 	flag.StringVar(&cfg.Password, "password", "", "SFTP host password")
 	flag.StringVar(&cfg.KeyPath, "keypath", "", "SFTP host key file path")
 	flag.StringVar(&cfg.ListenAddress, "listen", ":8080", "HTTP listen address")
