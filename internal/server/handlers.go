7c984a68dc77f2c5d69648ef129fb4169bff493c
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-20 20:18:12 +0100

    feat: handleRemoteList

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -3,6 +3,8 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+
+	"sftptrans/internal/session"
 )
 
 type APIResponse struct {
@@ -31,4 +33,19 @@ func writeError(writer http.ResponseWriter, status int, message string, code str
 
 func writeSuccess(writer http.ResponseWriter, data any) {
 	writeJSON(writer, http.StatusOK, APIResponse{Success: true, Data: data})
+}
+
+func handleRemoteList(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Query().Get("path")
+	if path == "" {
+		path = "/"
+	}
+
+	sess := session.Current()
+	entries, err := sess.Client().ListDir(path)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error(), "SFTP_LIST_ERROR")
+		return
+	}
+	writeSuccess(w, entries)
 }
