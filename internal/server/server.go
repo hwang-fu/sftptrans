037b75018573c4e200ee84c8a41e6898ec2f7a81
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-20 20:40:04 +0100

    feat: ServeHTTP

--- internal/server/server.go
+++ internal/server/server.go
@@ -20,6 +20,7 @@ func NewServer(listenAddr string) *http.Server {
 	if err != nil {
 		slog.Error("Failed to load static files", "error", err)
 	}
+	mux.Handle("/", spaHandler{staticFS: http.FileServer(http.FS(staticFS))})
 
 	return &http.Server{}
 }
@@ -27,4 +28,9 @@ func NewServer(listenAddr string) *http.Server {
 // spaHandler serves static files and falls back to index.html for SPA routing
 type spaHandler struct {
 	staticFS http.Handler
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// try to serve the file
+	h.staticFS.ServeHTTP(w, r)
 }
