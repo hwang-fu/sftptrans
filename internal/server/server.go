508d2be5f81f4bbd1f7b4d3347e390eafc18cb3c
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-20 20:37:40 +0100

    feat: spaHandler struct

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,2 +1,30 @@
 package server
 
+import (
+	"embed"
+	"io/fs"
+	"log/slog"
+	"net/http"
+)
+
+var staticFiles embed.FS
+
+func NewServer(listenAddr string) *http.Server {
+	mux := http.NewServeMux()
+
+	// API routes
+	mux.HandleFunc("", handleRemoteList)
+
+	// Static files (Angular SPA)
+	staticFS, err := fs.Sub(staticFiles, "static")
+	if err != nil {
+		slog.Error("Failed to load static files", "error", err)
+	}
+
+	return &http.Server{}
+}
+
+// spaHandler serves static files and falls back to index.html for SPA routing
+type spaHandler struct {
+	staticFS http.Handler
+}
